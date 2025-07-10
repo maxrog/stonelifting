84fe609ee525293c4f99c96eff704fbf51588081
Max Rogers <max.rogers@yinzcam.com>
2025-07-10 05:59:29 -0400

    add Xcode proj

---
+++ iOS/StoneLifting/StoneLifting/App/StoneLiftingApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoneLiftingApp.swift
+//  StoneLifting
+//
+//  Created by Max Rogers on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct StoneLiftingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/StoneLifting/StoneLiftingTests/StoneLiftingTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoneLiftingTests.swift
+//  StoneLiftingTests
+//
+//  Created by Max Rogers on 7/10/25.
+//
+
+import Testing
+@testable import StoneLifting
+
+struct StoneLiftingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
