75392cf77b57e0272d752c8833bb2ac0c5076da2
Max Rogers <max.rogers@yinzcam.com>
2025-11-06 07:55:57 -0500

    update TODOS

--- iOS/StoneLifting/StoneLifting/App/StoneLiftingApp.swift
+++ iOS/StoneLifting/StoneLifting/App/StoneLiftingApp.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 /*
  TODO:
+ - icon8 system sf icons
+ or
  􁊕 feature circle.dotted.and.circle in app, rotated so dotted is vertical and with wiggle animation (palette / hierarchical coloring with accent) - wiggle makes it seem like you're picking it up
+ Design make less AI - ish pretty obvious
+ Claude help document git repo etc?
+ AI sparkle thing for stone weight estimation
  App Name: Go On
  Subtitle: Pick it up
  Badges for half/full sterker 
